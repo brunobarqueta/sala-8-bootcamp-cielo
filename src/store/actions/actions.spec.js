a9580c39cea19544207168d7800686e27d290470
Cledson Leite <csbetsonline@gmail.com>
2023-09-20 13:47:44 -0300

    fix: ajustment test and actions

--- src/store/actions/actions.spec.js
+++ src/store/actions/actions.spec.js
@@ -1,16 +1,27 @@
+import { faker } from "@faker-js/faker";
 import { listFailure, listRequired, listSuccess } from "./";
 
-describe('Actions', () => {
+describe("Actions", () => {
   it("Should return type required list", () => {
-    const result = listRequired()
+    const result = listRequired();
     expect(result).toEqual({ type: "list_required" });
   });
   it("Should return failed list type", () => {
-    const result = listFailure();
-    expect(result).toEqual({ type: "list_failure" });
+    const error = faker.lorem.sentence(5);
+    const result = listFailure(error);
+    expect(result).toEqual({ type: "list_failure", payload: error });
   });
   it("Should return success list type", () => {
-    const result = listSuccess();
-    expect(result).toEqual({ type: "list_success" });
+    const productFake = {
+      id: faker.string.uuid(),
+      name: faker.commerce.productName(),
+      avatar: faker.image.url(),
+      description: faker.commerce.productDescription(),
+      price: faker.commerce.price(),
+      rating: faker.number.float({min: 1.0, max: 5.0}),
+      category: faker.commerce.department()
+    }
+    const result = listSuccess([productFake]);
+    expect(result).toEqual({ type: "list_success", payload: [productFake] });
   });
 });
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -8,10 +8,12 @@ export const listRequired = () => ({
     type: Types.LIST_REQUIRED
 })
 
-export const listFailure = () => ({
-    type: Types.LIST_FAILURE
+export const listFailure = (payload) => ({
+    type: Types.LIST_FAILURE,
+    payload
 })
 
-export const listSuccess = () => ({
-    type: Types.LIST_SUCCESS
+export const listSuccess = (payload) => ({
+    type: Types.LIST_SUCCESS,
+    payload
 })
